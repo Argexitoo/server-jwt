5bec8094e52c00ad6d7f976801868fb37643f8c7
Argexitoo <argexitoo@gmail.com>
2022-02-27 18:43:31 +0100

    models user,dog,meeting done

---
+++ models/Dog.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const dogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sex: {
+    type: String,
+    required: true,
+    enum: ['Female', 'Male'],
+  },
+  race: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ['Small', 'Medium', 'Large', 'Giant'],
+  },
+  age: {
+    type: String,
+    required: true,
+    enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
+  },
+  image: { type: String },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
+});
+
+const Dog = mongoose.model('Dog', dogSchema);
+
+module.exports = Dog;
--- models/User.model.js
+++ models/User.model.js
@@ -4,6 +4,18 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  password2: {
+    type: String,
+    required: [true, 'password is required'],
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
   name: { type: String, required: true },
 });
 
