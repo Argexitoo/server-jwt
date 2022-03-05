d10491fdd3c7a26ad5bb6676a8c4838c37560fdc
Argexitoo <argexitoo@gmail.com>
2022-03-05 10:46:46 +0100

    fixed bugs

--- models/User.model.js
+++ models/User.model.js
@@ -4,10 +4,6 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  password2: {
-    type: String,
-    required: [true, 'password is required'],
-  },
   location: {
     type: String,
     required: true,
